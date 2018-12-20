c7e9a4c35364ee3df641b62baeb34bd25e0531cf
Clément Cottet <clement.cottet.pro@gmail.com>
2018-12-20 08:47:16 +0100

    Moving linechart in a specific div

--- script_temperature.js
+++ script_temperature.js
@@ -1,15 +1,15 @@
 region = 7149;
 
-d3.select("body")
+d3.select("body").select("#linechart")
     .append("h2")
-    .attr("id", "title")
+    .attr("id", "linechartTitle")
     .text("Temperature in station n°" + region + ".");
 
 var margin = {top: 20, right: 20, bottom: 50, left: 70},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("body").select("#linechart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -184,21 +184,7 @@ function refreshView(region) {
 
         });
 
-        // Change the line
-        // Ordinal scale
-        /*var x = d3.scaleTime()
-            .range([0, width])
-            .domain(d3.extent(data, function (d) {
-                return d.date;
-            }));
-
-        // Linear scale
-        var y = d3.scaleLinear()
-            .range([0, height])
-            .domain(d3.extent(data, function (d) {
-                return d.temperature;
-            }));*/
-
+        //Change the line
         var line = d3.line()
             .x(function (d) {
                 return x(d.date);
