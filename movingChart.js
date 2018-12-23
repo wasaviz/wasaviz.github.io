ef63532ca90232055ae130e402bb9198c1b9e91c
OceaneCsn <oceane.cassan@insa-lyon.fr>
2018-12-23 12:43:00 +0100

    graph added to index.html with two variables to choose

--- movingChart.js
+++ movingChart.js
@@ -1,63 +1,155 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="utf-8">
-  <script src="https://d3js.org/d3.v4.min.js"></script>
-  <style>
-    body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; }
-    
-    .hidden {
-        display: none;
-    }
-    div.tooltip {
-        color: #222;
-        background-color: #fff;
-        padding: .5em;
-        text-shadow: #f5f5f5 0 1px 0;
-        border-radius: 2px;
-        opacity: 0.9;
-        position: absolute;
-    }
-  </style>
-</head>
+  
+  
+var margin = {top: 20, right: 20, bottom: 50, left: 70}
+var width = 800 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
-<body>
-  <script>
-		var width = 700,
-  		  height = 580;
+var svg = d3.select("body").select("#regiongraph").append("svg")
+  .attr( "width", width + margin.left + margin.right)
+  .attr( "height", height +margin.top + margin.bottom+50)
+  .append("g")
+.attr("transform",
+	"translate(" + margin.left + "," + margin.top + ")");
+	
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+var x = d3.scaleLinear()
+var y = d3.scaleLinear()
+var y2 = d3.scaleLinear()
+var x_axis;
+var y_axis;
 
-		var svg = d3.select( "body" )
-  		.append( "svg" )
-		  .attr( "width", width )
-		  .attr( "height", height );
-		  
+d3.csv("newData.csv", function(data) {
+	
+	
+	//console.log(data[Temperature)
+	
+			//.domain([d3.min(data.Temperature),d3.max(data.Temperature)])
+			x.domain([-20, 40])
+			.range([0,width]);
+			
+	
+			y.domain([-0.1, 30])
+			.range([0,height]);
+		
+	x_axis = d3.axisBottom(x);
 
-    d3.csv("newData.csv", function(data) {
-    
-		var x = d3.scale.linear()
-				.domain([min(data.Temperature),max(data.Temperature)])
-				.range([0,width]);
+	
+			
+	
+	y2.domain([-0.1, 30])
+	.range([height,0]);
+			
+	y_axis = d3.axisLeft(y2);
+
+  svg.selectAll("rect")
+	.data(data)
+	.enter()
+	.append("rect")
+	.style("fill", function(d) {
+		return color(d.IDStation);
+	  })
+	.attr('width', 10)
+	.attr('height', 10)
+	.attr('x', function(d){
+		return x(d.Temperature)+margin.left ;
+	})
+	.attr('y', function(d){
+		return height - y(d.Pluie24);
+	})
+	.on("mouseover", function(d){
+		console.log("hey")
+		d3.selectAll("rect").filter(function(e) {
+			
+		return e === d;
+	  })
+	  .transition()
+	  .attr('width', 20)
+	  .attr('height', 20)
+	})
 
 
-		var y = d3.scale.linear()
-				.domain([min(data.Pluie24),max(data.Pluie24)])
-				.range([0,height]);
-    
-      svg.selectAll("rect")
-      	.data(data)
-      	.enter()
-      	.append("rect")
-        .style("fill", function(d) {
-			return 'red';
+	.on("mouseout", function(d){
+	  d3.selectAll("rect")
+	  .transition()
+	  .attr('width', 10)
+	  .attr('height', 10)
+	})
+	.style('stroke-width', 0.5)
+	.style('stroke', 'black');
+	
+	svg.append('g').attr('transform', 'translate('+margin.right+', 0 )').call(y_axis);
+
+	svg.append('g')
+	.attr('transform', 'translate(0,' +450+ ')')
+	.attr('class', 'x axis')
+	.call(x_axis);
+});
+
+	
+function createGraph(var_x, var_y){
+	
+	d3.csv("newData.csv", function(data) {
+		//console.log(d3.max(data[var_x]))
+		x.domain(d3.extent(data, function(d){
+			return +d[var_x];
+		}))
+		//.range([0,width]);
+
+		y.domain(d3.extent(data, function(d){
+			return +d[var_y]
+		}))
+		
+		console.log(d3.extent(data, function(d){
+			return +d[var_y]
+		}))
+		//.range(0,height);
+		
+		x_axis.scale(x);
+		y2.domain(d3.extent(data, function(d){
+			return +d[var_y];
+		}))
+		//.range(height, 0);
+		
+		y_axis.scale(y2);
+	
+	  svg.selectAll("rect")
+		.data(data)
+		.style("fill", function(d) {
+			return color(d.IDStation);
 		  })
-      	.attr('width', 30)
-      	.attr('height', 30)
-      	.attr('x', function(d){
-			return x(d.Temerature) );
+		.transition()
+		.attr('width', 10)
+		.attr('height', 10)
+		.attr('x', function(d){
+			return x(d[var_x]) ;
 		})
-      	.attr('y', function(d){
-			return y(d.Pluie24);
+		.attr('y', function(d){
+			return height - y(d[var_y]);
 		})
-    });
+		.on("mouseover", function(d){
+			d3.selectAll("rect").filter(function(e) {
+			return e === d;
+		  })
+		  .transition()
+		  .attr('width', 20)
+		  .attr('height', 20)
+		})
+		.on("mouseout", function(d){
+		  d3.selectAll("rect")
+		  .transition()
+		  .attr('width', 10)
+		  .attr('height', 10)
+		});
+		
+		svg.append('g').attr('transform', 'translate('+margin.right+', 0 )').call(y_axis);
 
-  </script>
-</body>
+		svg.append('g')
+		.attr('transform', 'translate(0,' +450+ ')')
+		.attr('class', 'x axis')
+		.call(x_axis);
+	})
+}
+
+	
+
+
